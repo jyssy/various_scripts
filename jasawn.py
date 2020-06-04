c14d4cca51631380e3b6419e61a343e312ed1118
Jesse L <30397506+jyssy@users.noreply.github.com>
2020-06-04 12:01:30 -0500

    file-extension changes

---
+++ jasawn.py
@@ -0,0 +1,13 @@
+import os, sys
+
+path = 'C:/Users/jal!/Downloads/Sinopiadata/'
+
+for file in os.listdir(path):
+	#print(file)
+	if file != "complete.log" and file != "jasawn.py":
+		os.chdir('C:/Users/jal!/Downloads/Sinopiadata/')
+		file = (file)
+		filename = file + ".json"
+		filename = open(filename,'w')
+
+		# content is being deleted from the files (why, oh why?)
