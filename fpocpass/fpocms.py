7566d016e306586fcd97d2f0224afc1de25fff6d
jyssy <jessel@phantomlandscapes.com>
2021-05-04 12:13:16 -0400

    more script changes

--- fpocpass/fpocms.py
+++
@@ -1,6 +0,0 @@
-
-
-exec(open('fpocpass.py').read())
-exec(open('fastwb.py').read())
-exec(open('osRemove.py').read())
-
--- fpocpass/fpocpass.py
+++ fpocpass/fpocpass.py
@@ -1,34 +1,10 @@
 # looking at yes(s) and no(s)
 
-import re
+import pandas as pd
 
-no = []
-yes = []
+file = '2021FASTSurveyResponses.csv'
+df = pd.read_csv(file)
+# print(df.columns)
+yesno = pd.value_counts(df['yesno'])
 
-with open('2021FASTSurveyResponses.csv', 'r') as fastout:
-    yayornay = fastout.readlines()
-
-    for line in yayornay:
-        line = line[1]
-        pattern1 = re.compile(r"no")
-        pattern2 = re.compile(r"yes")
-        pattern1.split(',')
-        pattern2.split(',')
-        # print(pattern2)
-
-        if 'no' in line:
-            no.append(line)
-
-            print(no)
-
-        if 'yes' in line:
-            yes.append(line)
-
-    #     if 'yes' in line:
-    #         yes.append(numbers)
-    #     if 'no' in line:
-    #         no.append(numbers)
-
-# with open('yayornay.csv', 'w') as f:
-#     f.write('Yes(s) = {} /'.format(yes)
-#     f.write(' No(s) = {} '.format(no)
+print(yesno)
