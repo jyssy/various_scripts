edee912ac6aceea9de4fe821dd72f8313968c241
jyssy <jessel@phantomlandscapes.com>
2021-05-20 23:43:32 -0500

    bash play

---
+++ BaSH/cpumem.sh
@@ -0,0 +1,22 @@
+# CPU and Memory information
+
+cat /proc/cpuinfo
+
+sleep 3
+
+cat /proc/meminfo
+
+print "There is your CPU and memory information."
+
+sleep 3
+
+du -ah 
+
+sleep 2
+
+
+
+
+
+
+
