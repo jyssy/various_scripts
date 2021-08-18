37e696d197c53791c8c9cad9b81026d1204f6e92
Jesse L <30397506+jyssy@users.noreply.github.com>
2021-08-18 10:26:38 -0500

    Update Law_Statsv2.sql

--- sql/Law_Statsv2.sql
+++ sql/Law_Statsv2.sql
@@ -1,5 +1,5 @@
 -- Tidier Law report for J -- item notes
-select
+SELECT
     b.bib_id 'BibID',
     b.status 'BibStatus',
     h.holdings_id 'HoldingsID',
@@ -8,28 +8,28 @@ select
     i.updated_by 'UpdatedBy',
     i.date_updated 'DateUpdated',
     n.note 'ItemNote'
-from ole_ds_bib_t b
-join ole_ds_holdings_t h on b.bib_id = h.bib_id
-join ole_ds_item_t i on h.holdings_id = i.holdings_id
-join ole_ds_item_note_t n on i.item_id = n.item_id
-where 
-    h.location like '%Law%' and 
-    n.type = 'nonPublic' and 
-    n.note regexp '^[[:alpha:]]{6}$' and 
-    n.note not in ('folder', 'binder', 'SLAVEX', 'NISORI') and
-    i.date_created between '2021-06-01' and '2021-07-01';
+FROM ole_ds_bib_t b
+JOIN ole_ds_holdings_t h ON b.bib_id = h.bib_id
+JOIN ole_ds_item_t i ON h.holdings_id = i.holdings_id
+JOIN ole_ds_item_note_t n ON i.item_id = n.item_id
+WHERE 
+    h.location LIKE '%Law%' AND 
+    n.type = 'nonPublic' AND 
+    n.note regexp '^[[:alpha:]]{6}$' AND 
+    n.note NOT IN ('folder', 'binder', 'SLAVEX', 'NISORI') AND
+    i.date_created BETWEEN '2021-06-01' AND '2021-07-01';
     
     
 -- Tidier Law report for J - Bib 969 notes
-select
+SELECT
     b.bib_id 'Bib ID',
-    (select group_concat(m1.content) from cat.marc m1 where m.bib_id = m1.bib_id and m1.tag = 969) as '969',
+    (SELECT group_concat(m1.content) FROM cat.marc m1 WHERE m.bib_id = m1.bib_id AND m1.tag = 969) AS '969',
     h.call_number 'CallNumber',
     b.date_updated 'DateBibUpdated',
     b.updated_by 'BibUpdatedBy'
-from cat.marc m
-left join ole_ds_holdings_t h on m.bib_id = h.bib_id
-join ole_ds_bib_t b on m.bib_id = b.bib_id
-where 
-    m.tag = 969 and
-    b.date_updated between '2021-06-01' and '2021-07-01';
+FROM cat.marc m
+left JOIN ole_ds_holdings_t h ON m.bib_id = h.bib_id
+JOIN ole_ds_bib_t b ON m.bib_id = b.bib_id
+WHERE 
+    m.tag = 969 AND
+    b.date_updated BETWEEN '2021-06-01' AND '2021-07-01';
