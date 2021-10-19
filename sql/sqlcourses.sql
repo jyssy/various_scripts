c8100232ef966041bc5b21fa3a0ed4df66ee5eab
jyssy <jessel@phantomlandscapes.com>
2021-10-19 14:42:16 -0400

    more notes on sql

--- sql/sqlcourses.sql
+++ sql/sqlcourses.sql
@@ -8,7 +8,41 @@ GROUP BY(orders.customerid)
 ORDER BY COUNT(orders.customerid) desc;
 
 
+SELECT *
+FROM `countrylanguage`
+ORDER BY Percentage DESC;
 
 
+-- learning the unique MySQL set up in sequelace: https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
+
+SELECT * FROM Customers;
 
 
+SELECT CustomerName
+FROM Customers;
+
+SELECT * FROM products
+WHERE Price > 20;
+
+
+SELECT * FROM Customers
+WHERE Country = "Germany";
+
+
+SELECT COUNT(*) FROM Customers
+WHERE City = "London";
+
+
+SELECT * FROM Customers
+JOIN Orders on orders.customerid=customers.customerid
+WHERE Country = "Mexico";
+
+
+Select * FROM Products
+JOIN Categories ON categories.categoryid=products.categoryid
+WHERE ProductName = "Chang";
+
+
+SELECT *
+FROM `countrylanguage`
+ORDER BY countrycode ASC;
