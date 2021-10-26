9bb4109a3af11b91d016e90370f246bcb236b36e
jyssy <jessel@phantomlandscapes.com>
2021-10-26 11:51:50 -0400

    adding files for new projects

---
+++ baSH/copyrosterpy.sh
@@ -0,0 +1,4 @@
+# Copying the progress of this file into My github profile.
+# I can't include the files in the directory because of some privacy issues
+
+cp newroster.py ~/Docu*/Git*/various*/python
---
+++ python/newroster.py
@@ -0,0 +1,34 @@
+# new student rosters scripts
+
+import csv
+import operator
+import pandas as pd
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.workbook import Workbook
+
+wb = Workbook()
+ws1 = wb.create_sheet('Sheet1', 0)
+
+with open('extract_14523301.csv', 'r') as iuiestdnts:
+	for line in iuiestdnts:
+		print(line)
+
+	column1 = []
+	column2 = []
+	column3 = []
+	column4 = []
+	column5 = []
+	column6 = []
+	column7 = []
+	column8 = []
+	column9 = []
+	column10 = []
+	column11 = []
+	column12 = []
+
+
+
+print('healthy py')
+
+
+
