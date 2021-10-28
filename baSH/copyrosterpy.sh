0888704b06c08d65ed6673579f4b28bcd87b8ec1
jyssy <jessel@phantomlandscapes.com>
2021-10-28 16:07:52 -0400

    edited bash script

--- baSH/copyrosterpy.sh
+++ baSH/copyrosterpy.sh
@@ -1,5 +1,5 @@
 # Copying the progress of this file into My github profile.
 # I can't include the files in the directory because of some privacy issues
 
-cp newroster.py ~/Docu*/Git*/various*/python
-cp pynotesetc.txt ~/Docu*/Git*/various*/python
+cp newroster.py ~/Docu*/Git*/stsextract
+cp pynotesetc.txt ~/Docu*/Git*/stsextract
