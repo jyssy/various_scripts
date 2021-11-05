73236e369cb7fa9265cc181d045a94324fac6dac
jyssy <jessel@phantomlandscapes.com>
2021-11-05 07:59:16 -0400

    adding a URL

--- python/chext.py
+++ python/chext.py
@@ -1,5 +1,6 @@
 # this little script changes file extension names (in this case, jpeg to jpg, but it could be anything)
 # thank you to ghostdog74 on bytes.com
+# https://bytes.com/topic/python/answers/612797-change-file-extension
 
 import os, glob
 
