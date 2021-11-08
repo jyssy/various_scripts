b541eaa1f73109bbba4b55fca1cea77011ca4704
jyssy <jessel@phantomlandscapes.com>
2021-11-08 15:41:37 -0500

    new vid example of split.join

--- python/splitting.py
+++ python/splitting.py
@@ -12,3 +12,5 @@ print(joined)
 #strings2 = string.join(' ')
 with open('split.txt', 'w') as joinedtext:
 	joinedtext.write(joined)
+	
+# youtube vid on splitting and joining: https://www.youtube.com/watch?v=2WYR0XCoTAo
