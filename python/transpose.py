9a7f3f29addadc10e849033e41b981337c62211d
Jesse L <30397506+jyssy@users.noreply.github.com>
2023-05-01 11:18:16 -0400

    Add files via upload
    updated index values to drop columns before transposing

--- python/transpose.py
+++ python/transpose.py
@@ -1,9 +1,36 @@
 
 import pandas as pd
 
-df = pd.read_csv('orgExport.csv')
-df = df.T
-df.to_csv('FireForm.csv')
+# opening the document
+studentorg = pd.read_csv('orgExport.csv')
+
+# dropping columns not needed
+studentorg.drop(studentorg.columns[[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56]], axis=1, inplace=True)
+
+# counting columns
+# items = [[20, 21]]
+# transposing
+studentorg = studentorg.T
+
+# output
+studentorg.to_excel('FFormResults_test2.xlsx')
+
+print('Student Org results ready')
+
+
+# transposing stuff below: 
+# opening the document
+# dftransposed = pd.read_csv('orgExport.csv')
+
+# # transposing
+# dftransposed = dftransposed.T
+
+# # output
+# dftransposed.to_excel('FFormResults_test.xlsx')
+
+# print('Transposed document done')
+
+
 
 
 
