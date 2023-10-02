62be83af15e7a2513e7aebfc8e81c0637deb2a67
Jesse L <30397506+jyssy@users.noreply.github.com>
2023-10-02 10:58:39 -0400

    Add files via upload
    this version seems to be working much better

--- python/missingconversions.py
+++ python/missingconversions.py
@@ -7,20 +7,19 @@ docx_output_dir = '_examfilesdocx/'
 missing_conversions_file = 'missing_conversions.txt'
 
 # Get a list of all PDF file names
-pdf_files = set(os.listdir(pdf_source_dir))
+pdf_files = os.listdir(pdf_source_dir)
 
 # Get a list of all DOCX file names
-docx_files = set(os.listdir(docx_output_dir))
+docx_files = os.listdir(docx_output_dir)
 
 # Find PDF file names that do not exist as DOCX files
-missing_conversions = pdf_files - docx_files
+missing_conversions = [pdf_file for pdf_file in pdf_files if pdf_file.replace('.pdf', '.docx') not in docx_files]
 
 # Output the list of missing conversions to a text file
-if missing_conversions:
-    with open(missing_conversions_file, 'w') as file:
-        for missing_pdf in missing_conversions:
-            file.write(missing_pdf + '\n')
-    print(f"List of missing conversions saved in {missing_conversions_file}")
-else:
-    print("All PDFs have been converted to DOCX.")
+with open(missing_conversions_file, 'w') as file:
+    for missing_pdf in missing_conversions:
+        file.write(missing_pdf + '\n')
+
+print(f"List of missing conversions saved in {missing_conversions_file}")
+
 
