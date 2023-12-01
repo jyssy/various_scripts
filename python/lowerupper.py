addfee01a622153b08259eb893c3349ca1eb83ed
Jesse L <30397506+jyssy@users.noreply.github.com>
2023-12-01 15:24:53 -0500

    Add files via upload

--- python/lowerupper.py
+++ python/lowerupper.py
@@ -1,7 +1,7 @@
 
 # the input of what I want to change
 
-loworup = input("Do you want to make upper or lower case? Please enter only either 'lower' or 'upper'. ")
+loworup = input("Do you want to make upper or lower case? Please enter only either 'lower' or 'upper' or 'capitalize'. ")
 words = input("What are the words to change? ")
 
 if loworup == 'lower':
@@ -15,6 +15,13 @@ elif loworup == 'upper':
     # conditional for making upper case
     words = words.upper()
     with open('lower.txt', 'w'):
+        words.write(str(words))
+        print(words)
+
+if loworup == 'capitalize':
+    # conditional for capitalizing the first character
+    words = words.lower()
+    with open('capitalize.txt', 'w'):
         words.write(str(words))
         print(words)
 
