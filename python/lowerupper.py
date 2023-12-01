fdcc15ad010b00e9065a3451dadd1c63df22ce7f
Jesse L <30397506+jyssy@users.noreply.github.com>
2023-12-01 15:28:39 -0500

    Add files via upload

--- python/lowerupper.py
+++ python/lowerupper.py
@@ -8,14 +8,14 @@ if loworup == 'lower':
     # conditional for making upper case
     words = words.lower()
     with open('lower.txt', 'w'):
-        words.write(str(words))
+        words.write(str('words'))
         print(words)
 
 elif loworup == 'upper':
     # conditional for making upper case
     words = words.upper()
     with open('lower.txt', 'w'):
-        words.write(str(words))
+        words.write(str('words'))
         print(words)
 
 if loworup == 'capitalize':
