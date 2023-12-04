69c402b48333eb99cbd483827997bea6f7049233
Jesse L <30397506+jyssy@users.noreply.github.com>
2023-12-04 14:22:52 -0500

    new version with file object layer

--- python/lowerupper.py
+++ python/lowerupper.py
@@ -1,30 +1,34 @@
 
 # the input of what I want to change
 
-loworup = input("Do you want to make upper or lower case? Please enter only either 'lower' or 'upper' or 'capitalize'. ")
-words = input("What are the words to change? ")
+loworup = input("Do you want to make upper or lower case? Please enter only either 'low' or 'upp' or 'cap'. ")
+file_path = 'TextToChange.txt'
 
-if loworup == 'lower':
-    # conditional for making upper case
-    words = words.lower()
-    with open('lower.txt', 'w'):
-        words.write(str('words'))
+with open(file_path, 'r') as file:
+    content = file.read()
+
+if loworup == 'low':
+    # conditional for making lower case
+    words = content.lower()
+    with open('low.txt', 'w') as file:
+        file.writelines(words)
         print(words)
 
-elif loworup == 'upper':
+elif loworup == 'upp':
     # conditional for making upper case
-    words = words.upper()
-    with open('lower.txt', 'w'):
-        words.write(str('words'))
+    words = content.upper()
+    with open('upp.txt', 'w') as file:
+        file.writelines(words)
         print(words)
 
-if loworup == 'capitalize':
+elif loworup == 'cap':
     # conditional for capitalizing the first character
-    words = words.lower()
-    with open('capitalize.txt', 'w'):
-        words.write(str(words))
+    words = content.capitalize()
+    with open('cap.txt', 'w') as file:
+        file.writelines(words)
         print(words)
 
 else:
-    # printing the results
-    print("Please choose either 'lower' or 'upper'. ")
+    # printing an error message
+    print("Please choose either 'low', 'upp', or 'cap'.")
+
