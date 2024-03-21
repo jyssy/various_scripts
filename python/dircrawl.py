1a7b789255e1c37bd725fbfad32ac961a858ad7c
Jesse L <30397506+jyssy@users.noreply.github.com>
2024-03-21 09:46:51 -0400

    Update dircrawl.py

--- python/dircrawl.py
+++ python/dircrawl.py
@@ -13,16 +13,21 @@ def find_files_by_extension(extension, search_dirs):
     return found_files
 
 # List of directories to search in
-search_directories = [' [ add directory dhere ] ', ' [ add other directory here ] ']
+search_directories = ['DIRECTORY']
 
 # Extension to search for
-file_extension = '.cfm'
+file_extension = 'WCMS.cfm'
 
 # Find files with the specified extension
 result_files = find_files_by_extension(file_extension, search_directories)
 
+# Get the last directory name from the search directory path
+last_dir_name = os.path.basename(os.path.normpath(search_directories[0]))
+
+# Generate the output file name with the last directory name
+output_file = f"{last_dir_name}_search_results.json"
+
 # Output the results in a JSON file
-output_file = 'search_results.json'
 with open(output_file, 'w') as f:
     json.dump(result_files, f, indent=4)
 
