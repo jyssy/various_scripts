eecb1c4ab6b832d4c49fc56a4e0c73ebaba801ef
Jesse L <30397506+jyssy@users.noreply.github.com>
2025-05-21 07:51:14 -0400

    Delete docker_compose directory

--- docker_compose/serviceindex/db_manuals/db_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-PGPASSWORD='Bbfsjrvneru&fg' psql -U serviceindex_django -d serviceindex1 -f /docker-entrypoint-initdb.d/django.dump.1719417916copy.sql
--- docker_compose/serviceindex/db_manuals/init.sql
+++
@@ -1,4 +0,0 @@
--- init.sql
-
-GRANT ALL PRIVILEGES ON DATABASE serviceindex1 TO serviceindex_django;
-CREATE SCHEMA IF NOT EXISTS serviceindex AUTHORIZATION serviceindex_django;
